c2fba8fa186e7ae5757cacb3af5422c1cc55fc72
Chaiwa Berian <chaiwa.berian@gmail.com>
2016-11-09 22:43:24 +0200

    Remove route for /about

--- server.js
+++ server.js
@@ -3,12 +3,8 @@ var app = express();
 var path = require("path");
 
 app.get('/', function(request, response){
-	response.sendFile(path.join(__dirname+'/index.html'));
-	//__dirname will resolve to your project folder
-});
-
-app.get('/about', function(request, response){
-	response.sendFile(path.join(__dirname)+'/about.html'));
+	response.sendFile(path.join(__dirname+'/index.html')); //__dirname will resolve to your project folder
+	
 });
 
 app.listen(3000);
