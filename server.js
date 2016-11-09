e6cf45d9a1d8b2a083c87d64a7f21bfd1d2f61fa
Chaiwa Berian <chaiwa.berian@gmail.com>
2016-11-09 23:16:49 +0200

    Edit routing in server.js

--- server.js
+++ server.js
@@ -2,9 +2,15 @@ var express = require("express");
 var app = express();
 var path = require("path");
 
+app.set('port', (process.env.PORT || 5000));
+
+
+app.use(express.static(__dirname + '/public')); 		//public is directory for all static files like images, audio, css files , etc
+app.set('views', __dirname + '/views'); 				// views is directory for all template files
+
 app.get('/', function(request, response){
 	response.sendFile(path.join(__dirname+'/index.html')); //__dirname will resolve to your project folder
 	
 });
 
-app.listen(3000);
+app.listen(app.get('port'));
