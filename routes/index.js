47af5a5e0476376d4debb2616206cf245f586a07
Chaiwa Berian <chaiwa.berian@gmail.com>
2016-11-10 22:07:26 +0200

    Delete products folder

--- routes/index.js
+++ routes/index.js
@@ -3,7 +3,7 @@ var router = express.Router();
 
 /* GET home page. */
 router.get('/', function(req, res, next) {
-  res.render('index', { title: 'Silverkey Technologies -reliable technology reliable financing passionate innovation' });
+  res.render('index', { title: 'Silverkey Technologies:- reliable technology reliable financing passionate innovation' });
 });
 
 
