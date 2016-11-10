925b670d6e62b2676ed0667bd2df3ff455e43e25
Chaiwa Berian <chaiwa.berian@gmail.com>
2016-11-10 22:45:15 +0200

    Updated route and view for getting contact details

--- routes/contact.js
+++ routes/contact.js
@@ -2,7 +2,7 @@ var router = require('express').Router();
 
 /* GET Contact Page */
 router.get('/', function(req, res, next) {
-	res.render('contact', {title: "Silverkey Contact Details"});
+	res.render('contact', {title: "Contact Us"});
 });
 
 module.exports = router;
