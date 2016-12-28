31974b4396df83694a09f41a151facc6ecf1ccb0
Kena Kunda <kenakunda@gmail.com>
2016-12-28 12:40:31 +0200

    Deleted old express folders

--- routes/contact.js
+++
@@ -1,8 +0,0 @@
-var router = require('express').Router();
-
-/* GET Contact Page */
-router.get('/', function(req, res, next) {
-	res.render('contact', {title: "Contact Us"});
-});
-
-module.exports = router;
--- routes/downloads.js
+++
@@ -1,8 +0,0 @@
-var router = require('express').Router();
-
-/* GET Download Page */
-router.get('/', function(req, res, next) {
-	res.render('downloads', {title: "downloads"});
-});
-
-module.exports = router;
